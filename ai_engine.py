92b554192545deffcdbc84462b2aa648c7f3d488
Viktor Petrovic <viktorpetrovic@Viktors-MacBook-Air-6.local>
2026-08-16 16:26:05 -0400

    Added Try-Except method

--- ai_engine.py
+++ ai_engine.py
@@ -18,42 +18,48 @@ class ContactBook:
     def add_contact(self):
         name = input("Enter your name: ")
         phone = input("Enter your phone number: ")
-        age = int(input("Enter your age: "))
+        try:
+            age = int(input("Enter your age: "))
+        except ValueError:
+            return "You must provide an integer"
         new_contact = Contact(name, phone, age)
         self.contacts.append(new_contact)
-        return self.contacts
+        if not self.contacts:
+            return "No contact stored"
+        else:
+            return self.contacts
     
     def find_contact(self):
         name = input("Input the name you are looking for: ")
         for c in self.contacts:
             if c.name == name:
                 return c
-        return None
+        return "No name found"
 
     def delete_contact(self):
         name = input("Input the name you want to delete: ")
         for c in self.contacts:
             if c.name == name:
                 self.contacts.remove(c)
-        print("No such name found")
+                return
+        return "No such name found"
 
     def list_names(self):
         names = [c.name for c in self.contacts]
-        print(names)
+        return names
 
     def average_age(self):
         average = sum([c.age for c in self.contacts]) // len([c.age for c in self.contacts])
-        print(average)
+        return average
 
     def contacts_over(self):
         over = [c.name for c in self.contacts if c.age > 18 ]
-        print(F"IT'S OVER FOR {over}")
+        return f"IT'S OVER FOR {over}"
     
     def to_dict(self):
         todict = [{"name": c.name, "phone": c.phone, "age":c.age} for c in self.contacts]
-        print(todict)
         result = json.dumps(todict)
-        print(result)
+        return result
         
 book = ContactBook()
 
@@ -66,15 +72,15 @@ while True:
     elif choice == "2":
         print(book.find_contact())
     elif choice == "3":
-        book.delete_contact()
+        print(book.delete_contact())
     elif choice == "4":
         print(book.list_names())
     elif choice == "5":
         print(book.average_age())
     elif choice == "6":
-       book.contacts_over()
+       print(book.contacts_over())
     elif choice == "7":
-        book.to_dict()
+        print(book.to_dict())
     else:
         break
 
